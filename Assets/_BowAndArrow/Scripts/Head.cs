f1b8303614c7fe2a336e29fc66f3b44d60f1565b
leehb105 <59137572+leehb105@users.noreply.github.com>
2020-12-10 03:20:31 +0900

    20201210
    bow 추가

---
+++ Assets/_BowAndArrow/Scripts/Head.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Head : BodyPart, IDamageable // 머리는 몸에 상속되어 있다. 또한 데미지도 받는다.
+{
+    public new void Damage(int amount)
+    {
+        owner.TakeDamage(amount * 2); // 소유자(몸)에 대미지를 받는다 (그런데 2배 만큼 받는다)
+    }
+}
